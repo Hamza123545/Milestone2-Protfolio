af73162c16d721533e6d3227eac6090fa35f3859
Muhammad Hamza <hamzapersnol321@gmail.com>
2024-11-10 20:56:12 +0500

    deployment issue fixed

---
+++ next.config.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var nextConfig = {
+/* config options here */
+};
+// next.config.js
+module.exports = {
+    images: {
+        domains: ['via.placeholder.com', 'your-image-host.com'], // Add any other domains you might use here
+    },
+};
+exports.default = nextConfig;
